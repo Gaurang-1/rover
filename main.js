d7543ce9a94357d39c51b4a99770a384d7cf7278
Gaurang-1 <72252176+Gaurang-1@users.noreply.github.com>
2020-10-07 14:35:37 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -29,9 +29,9 @@ function uploadrover(){
     ctx.drawImage(rover_imageTag,rover_x,rover_y,rover_width,rover_height);
 }
 
-window.addEventListener("keydown",my_kew_is_down);
+window.addEventListener("keydown",my_keydown);
 
-function my_kew_is_down(){
+function my_keydown(e){
     keyPressed=e.keyCode;
     console.log(keyPressed);
 
@@ -50,5 +50,41 @@ function my_kew_is_down(){
     if (keyPressed =='39'){
         right();
         console.log("right");
+    }
+}
+
+function up(){
+    if (rover_y >=0){
+        rover_y-=10;
+        console.log("when up is pressed"+rover_x+"-"+rover_y);
+        uploadbackground();
+        uploadrover();
+    }
+}
+
+function down(){
+    if (rover_y<=500){
+        rover_y+=10;
+        console.log("when down is pressed, x="+rover_x+"| y="+rover_y);
+               uploadbackground();
+        uploadrover(); 
+    }
+}
+
+function left(){
+    if (rover_x>=0){
+        rover_x-=10;
+        console.log("when left is pressed, x="+rover_x+"| y"+rover_y);
+        uploadbackground();
+        uploadrover();
+    }
+}
+
+function right(){
+    if (rover_x<=700){
+        rover_x+=10;
+        console.log("when right is pressed, x="+rover_x+"| y"+rover_y);
+        uploadbackground();
+        uploadrover();
     }
 }
